87e9ee51cd8fbaed7c3a60e05bad2e0e814630bc
FabioIYT <37196600+FabioIYT@users.noreply.github.com>
2019-03-06 12:04:52 +0100

    Update 4zero4.php

--- 4zero4.php
+++ 4zero4.php
@@ -1,9 +1,22 @@
-<?php
-2	/*
-3	Plugin Name: 4zero4
-4	Plugin URI: https://www.inventyourtrade.it
-5	Description: Gestione pagina non trovata
-6	Author: DevFabio
-7	Version: 1.0
-8	Author URI: https://www.inventyourtrade.it
-9	*/
+/*
+Plugin Name: 4zero4
+Description: Gestione pagina non trovata, effettua una ricerca cercando termini relativi alla pagina assente
+Author: DevFabio
+Version: 1.0
+Author URI: https://www.inventyourtrade.it
+*/
+add_action('wp', 'p4zero4_redirect');
+function p4zero4_redirect()
+{
+        if(is_404()) 
+	        {
+	           $paginaMancante	=	urldecode(str_replace('-',' ',$_SERVER['REDIRECT_URL']));
+	           $ricerca	=	get_search_link( $paginaMancante );
+	          
+               
+                       header ('HTTP/1.1 301 Moved Permanently');
+                       header ("Location: " . $ricerca);
+                        exit(); 
+              
+        }
+	}
