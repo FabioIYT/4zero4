5415959cd204dae2bd4d0536c8f01eeaa9e2f71f
FabioIYT <37196600+FabioIYT@users.noreply.github.com>
2019-03-06 09:39:45 +0100

    Creazione file 4zero4.php

---
+++ 4zero4.php
@@ -0,0 +1,9 @@
+<?php
+2	/*
+3	Plugin Name: 4zero4
+4	Plugin URI: https://www.inventyourtrade.it
+5	Description: Gestione pagina non trovata
+6	Author: DevFabio
+7	Version: 1.0
+8	Author URI: https://www.inventyourtrade.it
+9	*/
