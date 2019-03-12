40028dddc2229783899c393b4bb45634739a2dab
mpceiv <45010533+mpceiv@users.noreply.github.com>
2019-03-12 07:57:02 +0000

    added wp function plus added info to plugin header

--- 4zero4.php
+++ 4zero4.php
@@ -1,22 +1,22 @@
-/*
-Plugin Name: 4zero4
-Description: Gestione pagina non trovata, effettua una ricerca cercando termini relativi alla pagina assente
-Author: DevFabio
-Version: 1.0
-Author URI: https://www.inventyourtrade.it
-*/
-add_action('wp', 'p4zero4_redirect');
-function p4zero4_redirect()
-{
-        if(is_404()) 
-	        {
-	           $paginaMancante	=	urldecode(str_replace('-',' ',$_SERVER['REDIRECT_URL']));
-	           $ricerca	=	get_search_link( $paginaMancante );
-	          
-               
-                       header ('HTTP/1.1 301 Moved Permanently');
-                       header ("Location: " . $ricerca);
-                        exit(); 
-              
-        }
+/**
+ * Plugin Name: 4zero4
+ * Plugin URI:  https://github.com/FabioIYT/4zero4
+ * Description: Gestione pagina non trovata, effettua una ricerca cercando termini relativi alla pagina assente
+ * Version:     1.0
+ * Author:      DevFabio
+ * Author URI:  https://www.inventyourtrade.it
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+add_action( 'wp', 'p4zero4_redirect' );
+function p4zero4_redirect() {
+	if ( is_404() ) {
+		$paginaMancante = urldecode( str_replace( '-', ' ', $_SERVER['REDIRECT_URL'] ) );
+		$ricerca        = get_search_link( $paginaMancante );
+
+		wp_redirect( $ricerca, 301 );
+		exit;
+
 	}
+}
